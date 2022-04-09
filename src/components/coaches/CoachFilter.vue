7f5907bfd8b0f857a2c2fd97e4ec947aa6028513
Fülöp Szilárd <fszili882@gmail.com>
2022-04-10 02:06:51 +0300

    coach, request, auth, ui components and pages

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,64 @@
+<template>
+  <base-card>
+    <h2>Find Your Coach</h2>
+    <span class="filter-option">
+      <input type="checkbox" id="powerlifting" checked @change="setFilter" />
+      <label for="powerlifting">Powerlifting</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="bodybuilding" checked @change="setFilter" />
+      <label for="bodybuilding">Bodybuilding</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="fitness" checked @change="setFilter" />
+      <label for="fitness">Fitness</label>
+    </span>
+  </base-card>
+</template>
+
+<script>
+export default {
+  emits: ['change-filter'],
+  data() {
+    return {
+      filters: {
+        powerlifting: true,
+        bodybuilding: true,
+        fitness: true,
+      },
+    };
+  },
+  methods: {
+    setFilter(event) {
+      const inputId = event.target.id;
+      const isActive = event.target.checked;
+      const updatedFilters = { ...this.filters, [inputId]: isActive };
+      this.filters = updatedFilters;
+      this.$emit('change-filter', updatedFilters);
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align: middle;
+}
+
+.filter-option label {
+  margin-left: 0.25rem;
+}
+
+.filter-option.active label {
+  font-weight: bold;
+}
+</style>
