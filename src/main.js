ef0bea42ee0558cd4f29c94396e987674f5dcf78
Fülöp Szilárd <fszili882@gmail.com>
2022-04-10 02:05:14 +0300

    lazy loading some components

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
+import router from './router';
+import store from './store/index';
+import App from './App';
+import BaseCard from './components/ui/BaseCard';
+import BaseButton from './components/ui/BaseButton';
+import BaseBadge from './components/ui/BaseBadge';
+import BaseSpinner from './components/ui/BaseSpinner';
 
-createApp({}).mount('#app');
+const BaseDialog = defineAsyncComponent(() =>
+  import('./components/ui/BaseDialog')
+);
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
+
+app.mount('#app');
